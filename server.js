9461b0d9ef14697ec3cb333a0e64545b17ad483d
TechBug2012 <mattbstanciu@gmail.com>
2018-05-05 22:34:14 -0400

    it all works!
    just need to add all of the states and then we're good to go for Tuesday!

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var stateFile = './state.json';
+var state = require(stateFile);
 var fs = require('fs');
 const r = require('rethinkdb');
 
@@ -19,19 +21,72 @@ fs.readFile('./cacert', function(err, caCert) {
   }, function(error, conn) {
     if (error) throw error;
     io.on('connection', function(socket) {
-      console.log("connected");
+      console.log("a user connected");
       socket.on('name', function(data) {
         r.db('eraproject').table('students').filter({"name" : data}).pluck("balance").run(conn, function(err, cursor) {
           if (err) throw err;
           cursor.toArray(function(err, result) {
             if (err) throw err;
+            console.log(data);
             socket.emit('balance', result[0].balance);
           });
         });
       });
-      r.db('eraproject').table('students').changes().run(conn, function(err, cursor) {
-        cursor.each(console.log);
+      socket.on('listenName', function(data) {
+        r.db('eraproject').table('students').filter(r.row('name').eq(data)).pluck("balance").changes().run(conn, function(err, cursor) {
+          if (err) throw err;
+          cursor.each(function(err, row) {
+            if (err) throw err;
+            var newbal = JSON.stringify(row.new_val, null, 2);
+            var newbal_parse = JSON.parse(newbal);
+            console.log("a change was made: " + data + ":" + newbal_parse.balance);
+            socket.emit('balance', newbal_parse.balance);
+          });
+        });
       });
+      socket.on('state', function(newstate) {
+        state.currentstate = newstate;
+        fs.writeFile(stateFile, JSON.stringify(state), function (err) {
+          if (err) return console.log(err);
+          console.log(JSON.stringify(state));
+          console.log('writing to ' + stateFile);
+        });
+      });
+      socket.on('user', function(data) {
+        fs.readFile(stateFile, 'utf8', function(err, state) {
+          if (err) throw err;
+          var currentState = JSON.parse(state);
+          console.log(currentState);
+          if (currentState["currentstate"] == "pay-50000") pay50000(data);
+          else if (currentState["currentstate"] == "pay-100000") pay100000(data);
+        });
+      });
+
+      function pushBalance(studentid) {
+        r.db('eraproject').table('students').get(studentid).pluck("balance").run(conn, function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          socket.emit('balance', result.balance);
+        });
+      }
+
+      function pay50000(studentid) {
+        var bal = 0;
+        r.db('eraproject').table('students').get(studentid).update({balance:r.row("balance").add(-50000)}).run(conn, function(err, result) {
+          if (err) throw err;
+          console.log("removed 50000 from " + studentid);
+          pushBalance(studentid);
+        });
+      }
+
+      function pay100000(studentid) {
+        var bal = 0;
+        r.db('eraproject').table('students').get(studentid).update({balance:r.row("balance").add(-100000)}).run(conn, function(err, result) {
+          if (err) throw err;
+          console.log("removed 100000 from " + studentid);
+          pushBalance(studentid);
+        });
+      }
     });
   });
 });
@@ -42,7 +97,7 @@ app.get("/", function (request, response) {
 app.get("/input", function (request, response) {
   response.sendFile(__dirname + '/views/input.html');
 });
-app.get("/state", function (request, response) {
+app.get("/statechange", function (request, response) {
   response.sendFile(__dirname + '/views/state-change.html');
 });
 app.get("/data", function (request, response) {
